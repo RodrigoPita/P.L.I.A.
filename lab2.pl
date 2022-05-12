b5ac7cb4cd041c7ec4b0b17468378be35354f481
Rodrigo Pita <41768439+RodrigoPita@users.noreply.github.com>
2022-05-12 16:29:44 -0300

    Update lab2.pl

--- lab2.pl
+++ lab2.pl
@@ -8,3 +8,13 @@
 %B) Corrija o programa
 min(X, Y, M):- X =< Y, !, M is X.
 min(X, Y, Y).
+
+tamanho([], 0).
+tamanho([_|Y], N):- tamanho(Y, N1), N is N1 + 1.
+
+par([]).
+par([_|T]):- impar(T).
+impar([_|T]):- par(T).
+
+lshift(L1, L2)
+rshift(L1, L2)
