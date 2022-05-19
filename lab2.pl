b8b14d9cbae25e8de4427a2b4dba5b7ad9271f30
Rodrigo Pita <41768439+RodrigoPita@users.noreply.github.com>
2022-05-19 00:45:08 -0300

    Consertando split/3

--- lab2.pl
+++ lab2.pl
@@ -42,7 +42,7 @@ nvogais([u], 1).
 nvogais([X], 0).
 nvogais([X|T], N):- nvogais([X], N1), nvogais(T, N2), N is N1 + N2.
 
-split([X], [X], []):- X < 0.
+split([X], [X], []):- X >= 0.
 split([X], [], [X]).
 split([X|T], P, N):- split([X], P1, N1), split(T, P2, N2), conc(P1, P2, P), conc(N1, N2, N).
 
