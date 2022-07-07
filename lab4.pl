83fbd8475225ac53281dd4c7d0053be26ea8581b
Rodrigo Pita <rodrigopita@dcc.ufrj.br>
2022-07-07 14:25:44 -0300

    Insert Sort implementado

--- lab4.pl
+++ lab4.pl
@@ -11,3 +11,10 @@ troca( [X, Y|T1], [Y|T2] ):- X > Y, troca( [X|T1], T2 ).
 
 bsort( L, L ):- troca( L, L ).
 bsort( L1, L2 ):- troca( L1, L3 ), bsort( L3, L2 ), !.
+
+insere( X, [], [X] ).
+insere( X, [Y|T], [X, Y|T] ):- X =< Y.
+insere( X, [Y|T], [Y|T] ):- X > Y, insere( X, T1, T2 ).
+
+isort( [], [] ).
+isort( [X|T], S ):- isort( T, T1 ), insere( X, T1, S ).
